9a20675600e148b1d4f41ce75a26f343bc912101
bryanlharris <48188000+bryanlharris@users.noreply.github.com>
2025-12-23 15:20:58 -0500

    Revert "Include dependencies when mirroring R packages"

--- r-build-mirror.R
+++ r-build-mirror.R
@@ -64,19 +64,7 @@ mirror_packages <- function(requirements_path, destination) {
     dir.create(mirror_dir, recursive = TRUE, showWarnings = FALSE)
   }
 
-  available <- available.packages()
-  deps <- tools::package_dependencies(
-    packages,
-    db = available,
-    recursive = TRUE
-  )
-  all_pkgs <- unique(c(packages, unlist(deps)))
-
-  download.packages(
-    pkgs = all_pkgs,
-    destdir = mirror_dir,
-    type = "win.binary"
-  )
+  download.packages(pkgs = packages, destdir = mirror_dir, type = "win.binary")
   tools::write_PACKAGES(dir = mirror_dir, type = "win.binary")
 }
 
